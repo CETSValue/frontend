c6d75d0ab6b673c9e3c9f17465956a0d9e9d29a5
Eugene Kulesha <eugene.kulesha@nanoporetech.com>
2025-10-29 16:44:27 +0000

    only allow authorized users

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useUser } from "@auth0/nextjs-auth0"
 
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
@@ -15,9 +14,44 @@ import {
 
 import data from "./data.json"
 
+import { useUser } from "@auth0/nextjs-auth0"
+
+const AllowedUsers: string[] = [
+  'kulesha@gmail.com'
+]
+
 export default function Page() {
   const { user, isLoading } = useUser();
-  console.log(user);
+
+  console.log(user)
+  if (user && user.email) {
+    if (AllowedUsers.indexOf(user.email) < 0 ) {
+    return (
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                <div className="px-4 lg:px-6">
+                  You are not allowed to access this page (signed in as <span className="font-bold">{user?.email ?? 'unknown user'}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    )
+    }
+  }
 
   return (
     <SidebarProvider

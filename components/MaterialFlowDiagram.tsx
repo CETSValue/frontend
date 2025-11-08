d9782f452a03b598fe6aed5fa872419d9294a8fc
Eugene Kulesha <eugene.kulesha@nanoporetech.com>
2025-11-08 16:40:56 +0000

    version bump

--- components/MaterialFlowDiagram.tsx
+++ components/MaterialFlowDiagram.tsx
@@ -37,7 +37,7 @@ interface NodeParams {
     data: Node
 }
 
-const lastUpdated = "08 Nov 2025 15:28";
+const lastUpdated = "08 Nov 2025 16:40";
 
 // ---- Custom node component with icon + label ----
 const CustomNode = ({ data }: NodeParams) => (

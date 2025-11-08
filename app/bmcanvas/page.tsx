a9a1bb34e5a00f522bc172616c1146275b50b434
Eugene Kulesha <eugene.kulesha@nanoporetech.com>
2025-11-08 15:32:37 +0000

    tidy up

--- app/bmcanvas/page.tsx
+++ app/bmcanvas/page.tsx
@@ -1,15 +1,8 @@
 'use client';
 
 import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
-import SustainabilityWidget from "@/components/chart"
-import ResilienceTreeExplorer
- from "@/components/resilience-tree-explorer";
-import ResilienceRadarExplorer
- from "@/components/resilience-radar-explorer";
+
 import {
   SidebarInset,
   SidebarProvider,
